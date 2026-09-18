0f7fd643014227deaceaaf47a91d163b245c604f
dclason24 <104878116+dclason24@users.noreply.github.com>
2026-09-18 17:19:47 -0400

    correcting database connection
    now it will display data from database (just userInfo so far to test)

--- public/firebase.js
+++ public/firebase.js
@@ -1,22 +1,36 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
+  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDf2OfUJFJPbHyJJgLRpYMCvspvoXhEirs",
-  authDomain: "capstone-project-15eaf.firebaseapp.com",
-  databaseURL: "https://capstone-project-15eaf-default-rtdb.firebaseio.com",
-  projectId: "capstone-project-15eaf",
-  storageBucket: "capstone-project-15eaf.firebasestorage.app",
-  messagingSenderId: "609118337266",
-  appId: "1:609118337266:web:bd64b747e398de725f386b",
-  measurementId: "G-R6KLBY2H6M"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDf2OfUJFJPbHyJJgLRpYMCvspvoXhEirs",
+    authDomain: "capstone-project-15eaf.firebaseapp.com",
+    databaseURL: "https://capstone-project-15eaf-default-rtdb.firebaseio.com",
+    projectId: "capstone-project-15eaf",
+    storageBucket: "capstone-project-15eaf.firebasestorage.app",
+    messagingSenderId: "609118337266",
+    appId: "1:609118337266:web:bd64b747e398de725f386b",
+    measurementId: "G-R6KLBY2H6M"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+
+const snapshot = await getDocs(collection(db, "userInfo"));
+const userDiv = document.getElementById("test");
+
+snapshot.forEach((doc) => {
+
+    const user = doc.data();
+
+    userDiv.innerHTML += `
+        <div>
+            <h2>${user.Name}</h2>
+            <p>Title: ${user.Title}</p>
+            <p>User ID: ${user.UserID}</p>
+        </div>
+    `;
+
+});
